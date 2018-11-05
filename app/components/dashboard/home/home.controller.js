c524c1585738f969957c9ca2854067d6b200e2ea
ATMECS\Himanshu.Nath <himanshu.nath@atmecs.com>
2018-11-05 17:52:43 +0530

    chatting phase 1 done

--- app/components/dashboard/home/home.controller.js
+++ app/components/dashboard/home/home.controller.js
@@ -1,10 +1,13 @@
 angular.module('chapp')
-  .controller('homeController', ['$state', '$rootScope', 'homeService', 'toastAlert', 'appConfig', 'localStorageService', 'SocketService', '$compile', '$scope',
-  function ($state, $rootScope, homeService, toastAlert, appConfig, localStorageService, SocketService, $compile, $scope) {
+  .controller('homeController', ['$state', '$scope', '$rootScope', 'homeService', 'toastAlert', 'appConfig', 'localStorageService', 'SocketService', '$compile', '$scope',
+  function ($state, $scope, $rootScope, homeService, toastAlert, appConfig, localStorageService, SocketService, $compile, $scope) {
     var vm = this;
     var toast = toastAlert;
+    let sendButtonFocus = false;
+    var TYPING_TIMER_LENGTH = 4000; // ms
 
     var userInfo = localStorageService.get("USER_PROFILE");
+    let male = userInfo.gender == 'Male';
 
     // const { senderId, text } = message
     
@@ -56,6 +59,15 @@ angular.module('chapp')
       // li.appendChild(document.createTextNode('amit'));
       // ul.appendChild(li);
     }
+
+    vm.joinGroupChat = function() {
+      vm.sendButtonFocus = true;
+      SocketService.emit('add user', userInfo.email);
+    }
+
+    vm.leaveGroupChat = function() {
+      
+    }
     
     // Sends a chat message
     const sendMessage = () => {
@@ -67,18 +79,11 @@ angular.module('chapp')
         });
         var sendData = {
           username: userInfo.email,
-          message: message
+          message: message,
+          gender: userInfo.gender
         };
         // tell server to execute 'new message' and send along one parameter
         SocketService.emit('new message', sendData);
-    }
-
-    vm.joinGroupChat = function() {
-      SocketService.emit('add user', userInfo.email);
-    }
-
-    vm.leaveGroupChat = function() {
-      
     }
 
     // function addMyChatMessage(data) {
@@ -88,21 +93,80 @@ angular.module('chapp')
     // }
 
     function addMyChatMessage(data) {
-      angular.element(document).find('#cardBody').append($compile("<div style='text-align: left;'><h5 class='card-title'>"+ data.username +"</h5><p class='card-text'>"+data.message +"</p></div>")($scope));
-      // angular.element(document).find('#cardBody').append($compile("<p class='card-text'>"+data.message +"</p></div>")($scope));
+      angular.element(document).find('#cardBody').append($compile("<div style='text-align: left;'><div class='row'><div class='col-sm-2'><img ng-if='"+male+"' src='assets/images/gender/boy.jpg' class='rounded-circle' alt='' width='70px'><img ng-if='!"+male+"' src='assets/images/gender/girl.jpg' class='rounded-circle' alt='' width='70px'></div><div class='col-sm-10'><h5 class='card-title'>"+ data.username +"</h5><p class='card-text'>"+data.message +"</p></div></div>")($scope));
       angular.element(document).find('#cardBody').append($compile("<hr>")($scope));
     }
 
     function addOtherChatMessage(data) {
-      angular.element(document).find('#cardBody').append($compile("<div style='text-align: right;'><h5 class='card-title'>"+ data.username +"</h5><p class='card-text'>"+data.message +"</p></div>")($scope));
-      // angular.element(document).find('#cardBody').append($compile("<p class='card-text'>"+data.message +"</p></div>")($scope));
+      let male = data.gender == 'Male';
+      angular.element(document).find('#cardBody').append($compile("<div style='text-align: right;'><div class='row'><div class='col-sm-10'><h5 class='card-title'>"+ data.username +"</h5><p class='card-text'>"+data.message +"</p></div><div class='col-sm-2'><img ng-if='"+male+"' src='assets/images/gender/boy.jpg' class='rounded-circle' alt='' width='70px'><img ng-if='!"+male+"' src='assets/images/gender/girl.jpg' class='rounded-circle' alt='' width='70px'></div></div></div>")($scope));
       angular.element(document).find('#cardBody').append($compile("<hr>")($scope));
     }
 
+    // Log a message
+    const log = (message, options) => {
+      var $el = $('<li>').addClass('log').text(message);
+      addMessageElement($el, options);
+    }
+
+    const addParticipantsMessage = (data) => {
+      var message = '';
+      if (data.numUsers === 1) {
+        message += "there's 1 participant";
+      } else {
+        message += "there are " + data.usersCount + " participants";
+      }
+      angular.element(document).find('#cardBody').append($compile("<p class='card-text' style='text-align: center;'><i>"+ message +"</i></p></div>")($scope));
+    }
+
+    // Adds the visual chat typing message
+    const addChatTyping = (data) => {
+      data.typing = true;
+      data.message = 'is typing';
+      addChatMessage(data);
+    }
+
+    // Whenever the server emits 'new message', update the chat body
     SocketService.on('new message', (data) => {
-      console.log("---------1");
-      console.log(data);
       addOtherChatMessage(data);
     });
+
+    // Whenever the server emits 'user joined', log it in the chat body
+    SocketService.on('user joined', (data) => {
+      angular.element(document).find('#cardBody').append($compile("<p class='card-text' style='text-align: center;'>"+data.username + "' joined'</p></div>")($scope));
+      addParticipantsMessage(data);
+    });
+
+    // Whenever the server emits 'user left', log it in the chat body
+    SocketService.on('user left', (data) => {
+      angular.element(document).find('#cardBody').append($compile("<p class='card-text' style='text-align: center;'>"+data.username + "' left'</p></div>")($scope));
+      addParticipantsMessage(data);
+    });
+
+    // Whenever the server emits 'typing', show the typing message
+    SocketService.on('typing', (data) => {
+      addChatTyping(data);
+    });
+
+    // Whenever the server emits 'stop typing', kill the typing message
+    SocketService.on('stop typing', (data) => {
+      removeChatTyping(data);
+    });
+
+    vm.whenTyping = function (event) {
+      SocketService.emit('typing');
+      lastTypingTime = (new Date()).getTime();
+      setTimeout(() => {
+        var typingTimer = (new Date()).getTime();
+        var timeDiff = typingTimer - lastTypingTime;
+        if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
+          socket.emit('stop typing');
+        }
+      }, TYPING_TIMER_LENGTH);
+    } 
+ 
+    vm.whenNotTyping = function (event) {
+
+    } 
 
   }]);
--- server/config/socket.js
+++ server/config/socket.js
@@ -1,4 +1,4 @@
-var numUsers = 0;
+var usersCount = 0;
 
 io.on('connection', (socket) => {
     console.log('user connected');
@@ -8,35 +8,54 @@ io.on('connection', (socket) => {
         // we tell the client to execute 'new message'
         socket.broadcast.emit('new message', {
           username: data.username,
-          message: data.message
+          message: data.message,
+          gender: data.gender
         });
         console.log(socket.username);
         console.log(data);
     });
 
-
     // when the client emits 'add user', this listens and executes
     socket.on('add user', (username) => {
-        socket.username = username;
-        ++numUsers;
+        console.log("---" + usersCount);
+        socket.username = username;        
+        ++usersCount;
+        console.log("--------" + usersCount);
         socket.emit('login', {
-        numUsers: numUsers
+            usersCount: usersCount
         });
+        console.log(usersCount);
         // echo globally (all clients) that a person has connected
         socket.broadcast.emit('user joined', {
         username: socket.username,
-        numUsers: numUsers
+        usersCount: usersCount
         });
     });
 
     // when the user disconnects.. perform this
     socket.on('disconnect', () => {
-        --numUsers;
+        console.log("=====" + usersCount);
+        --usersCount;
+        console.log("==========" + usersCount);
 
         // echo globally that this client has left
         socket.broadcast.emit('user left', {
             username: socket.username,
-            numUsers: numUsers
+            usersCount: usersCount
         });
     });
-});
+
+    // when the client emits 'typing', we broadcast it to others
+    socket.on('typing', () => {
+        socket.broadcast.emit('typing', {
+            username: socket.username
+        });
+    });
+
+    // when the client emits 'stop typing', we broadcast it to others
+    socket.on('stop typing', () => {
+        socket.broadcast.emit('stop typing', {
+            username: socket.username
+        });
+    });
+    });
